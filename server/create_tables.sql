f63e1de495531483f6b8e97b6460b96b5db6ef1c
James Ashe <jay@ashe.com>
2014-04-22 19:02:13 -0400

    Updated SQL for creating db

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS songs;
 DROP TABLE IF EXISTS queue;
 DROP TABLE IF EXISTS users;
 CREATE TABLE songs (id int, artist text, title text, album text, pi_owner text);
-CREATE TABLE queue (song_id int,  text);
+CREATE TABLE queue (index int, song_id int);
 CREATE TABLE users (name text, password text, roll text);
 INSERT INTO users VALUES('admin', 'password', 'admin');
