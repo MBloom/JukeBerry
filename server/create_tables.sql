7c4ff98d745ec2c6cc71b6e2a57de60e346b9240
Ubuntu <ubuntu@ip-172-31-39-61.us-west-2.compute.internal>
2014-04-22 18:57:53 +0000

    fixed forms

--- server/create_tables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS songs;
-DROP TABLE IF EXISTS queue;
-DROP TABLE IF EXISTS users;
-CREATE TABLE songs (id int, artist text, title text, album text, pi_owner text);
-CREATE TABLE queue (song_id int,  text);
-CREATE TABLE users (name text, password text, roll text);
-INSERT INTO users VALUES('admin', 'password', 'admin');
