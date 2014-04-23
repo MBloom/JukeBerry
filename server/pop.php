915c81257a6b8b28ba3f9dbe021b0f1b74260d6d
James Ashe <jay@ashe.com>
2014-04-23 16:15:15 -0400

    Fixed the weird error that made apache crash every second I think

--- server/pop.php
+++ server/pop.php
@@ -2,9 +2,9 @@
  $db = new SQLite3('jb.db');
  if ($_GET["raspID"] and $_GET["songID"]) {
  	$sql = "DELETE FROM queue WHERE pi_owner='" . $_GET["raspID"]. "' AND id=".$_GET["songID"].";";
- 	echo($sql);
+ 	//echo($sql);
  	$results = $db->query($sql);
- 	echo($results);
+ 	//echo($results);
  }
  
 ?>
