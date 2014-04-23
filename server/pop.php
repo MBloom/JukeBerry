4350256754fc149a4083f200c3eaa0d8d4a3eb12
James Ashe <jay@ashe.com>
2014-04-23 15:47:52 -0400

    Pop needs some different SQL

--- server/pop.php
+++ server/pop.php
@@ -1,7 +1,7 @@
 <?php
  $db = new SQLite3('jb.db');
- if ($_GET["raspID"]) {
- 	$sql = "DELETE FROM queue WHERE pi_owner='" . $_GET["raspID"]. "' LIMIT 1;";
+ if ($_GET["raspID"] and $_GET["songID"]) {
+ 	$sql = "DELETE FROM queue WHERE pi_owner='" . $_GET["raspID"]. "' AND id=".$_GET["songID"].";";
  	echo($sql);
  	$results = $db->query($sql);
  	echo($results);
