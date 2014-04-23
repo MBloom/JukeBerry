f1c9c5bdc3fd89ac388a3bc6f7c9a8547f77b104
James Ashe <jay@ashe.com>
2014-04-23 14:55:59 -0400

    Added pop and fixed stuff

---
+++ server/pop.php
@@ -0,0 +1,10 @@
+<?php
+ $db = new SQLite3('jb.db');
+ if ($_GET["raspID"]) {
+ 	$sql = "DELETE FROM queue WHERE pi_owner='" . $_GET["raspID"]. "' LIMIT 1;";
+ 	echo($sql);
+ 	$results = $db->query($sql);
+ 	echo($results);
+ }
+ 
+?>
