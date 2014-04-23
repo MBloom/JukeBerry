098b3c9c368de14192ea30eea7004ac6ff7c91a4
James Ashe <jay@ashe.com>
2014-04-22 22:30:51 -0400

    More testing on library upload

---
+++ server/upload.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="library.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="csv" id="csv"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
