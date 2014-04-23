0e1b5e99ca1992808d0d0ed6f043d3dc5a1cc8fb
James Ashe <jay@ashe.com>
2014-04-23 01:32:07 -0400

    Testing pop stuff

--- server/pop.php
+++ server/pop.php
@@ -4,6 +4,7 @@
 		$db = new SQLite3('jb.db');
 		if ($_GET["raspID"]) {
 			$sql = "DELETE FROM queue WHERE pi_owner='" . $_GET["raspID"]. "' LIMIT 1;";
+			echo($sql);
 			$results = $db->query($sql);
 		}
 		
