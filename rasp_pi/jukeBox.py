5c9c9a69521fe65adaf69934c111858e58339f27
atarnvik <pi@raspberrypi.(none)>
2014-04-24 01:56:53 +0000

    Raspberry pi side things.

--- rasp_pi/jukeBox.py
+++ rasp_pi/jukeBox.py
@@ -22,7 +22,7 @@ pygame.mixer.init()
 #queue.append(3)
 
 
-def getQueue():	
+def getQueueRepeat():	
 	lock.acquire()
 	print 'GETTING QUEUE!!!'
 	#Get queue here!!!!
@@ -30,35 +30,35 @@ def getQueue():
 		queue.clear()
 		payload = {'raspID' : 'cs4414projectpi'}
 		r = requests.get('http://54.186.3.95/queue.php', params=payload)
+		print 'Request send!'
 		seperate = r.content.split(',')
 		seperate[0] = seperate[0][4:]
 		for song in seperate:
-			queue.append(int(song))
+			if len(song) is not 0:
+				queue.append(int(song))
 		print queue
 	finally:
 		lock.release()
 	##################	
-	threading.Timer(30, getQueue).start()
+	threading.Timer(15, getQueueRepeat).start()
 
 
 
-getQueue()
-
-
-
+getQueueRepeat()
 
 while True:
 	if pygame.mixer.music.get_busy() == False and len(queue) is not 0:
 		lock.acquire()
 		try:
 			songInt = queue.popleft()
-			#sendInfo = {'songID': songInt}
+			sendInfo = {'songID': songInt, 'raspID' : 'cs4414projectpi'}
 			#send to server to pop songInt
-			#requests.get('ec2-54-186-3-95.us-west-2.compute.amazonaws.com/pop.php'	) 
+			requests.get('http://ec2-54-186-3-95.us-west-2.compute.amazonaws.com/pop.php', params = sendInfo) 
+			print 'requestSent'
 			songLoc = direct[songInt].strip()
 			pygame.mixer.music.load(songLoc)
 			pygame.mixer.music.play()
 		finally:
-			lock.release()
-	time.sleep(10)
-	pygame.mixer.music.stop()
+			lock.release()	
+		time.sleep(30)
+		pygame.mixer.music.stop()
